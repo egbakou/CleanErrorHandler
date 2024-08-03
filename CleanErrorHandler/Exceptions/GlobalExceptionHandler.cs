742ca124548af130f4490492d0a2c9900a872e53
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2024-08-03 13:32:36 +0200

    refactor: GlobalExceptionHandler refactored

--- CleanErrorHandler/Exceptions/GlobalExceptionHandler.cs
+++ CleanErrorHandler/Exceptions/GlobalExceptionHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
@@ -18,22 +19,47 @@ public class GlobalExceptionHandler(IHostEnvironment hostEnvironment, ILogger<Gl
     {
         Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
     };
+    
 
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var exceptionMessage = exception.Message;
+        logger.LogError(
+            "Error Message: {ExceptionMessage}, Time of occurrence {Time}",
+            exceptionMessage, DateTime.UtcNow);
+        // Return false to continue with the default behavior
+        // - or - return true to signal that this exception is handled
+        // return ValueTask.FromResult(false);
+        
+        var problem = CreateProblemDetails(httpContext, exception);
+        var json = ToJson(problem);
+        
+        const string contentType = "application/problem+json";
+        httpContext.Response.ContentType = contentType;
+        await httpContext.Response.WriteAsync(json, cancellationToken);
+
+        return true;
+    }
+    
     private ProblemDetails CreateProblemDetails(in HttpContext context, in Exception exception)
     {
         var problemDetails = new ProblemDetails
         {
             Status = StatusCodes.Status500InternalServerError,
             Title = "An error occurred",
+            Instance = context.Request.Path.Value,
+            Type = Helpers.GetProblemType(StatusCodes.Status500InternalServerError),
         };
+        
+        problemDetails.Extensions.Add("traceId", Activity.Current?.Id ?? context.TraceIdentifier);
 
         if (hostEnvironment.IsDevelopment())
         {
+            problemDetails.Detail = exception.ToString();
             return problemDetails;
         }
 
-        problemDetails.Detail = exception.ToString();
-        //problemDetails.Extensions["data"] = exception.Data;
+        problemDetails.Detail = "An error occurred while processing your request.";
         return problemDetails;
     }
 
@@ -50,26 +76,6 @@ public class GlobalExceptionHandler(IHostEnvironment hostEnvironment, ILogger<Gl
         }
 
         return string.Empty;
-    }
-
-    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
-    {
-        var exceptionMessage = exception.Message;
-        logger.LogError(
-            "Error Message: {exceptionMessage}, Time of occurrence {time}",
-            exceptionMessage, DateTime.UtcNow);
-        // Return false to continue with the default behavior
-        // - or - return true to signal that this exception is handled
-        // return ValueTask.FromResult(false);
-        
-        var problem = CreateProblemDetails(httpContext, exception);
-        var json = ToJson(problem);
-        
-        const string contentType = "application/problem+json";
-        httpContext.Response.ContentType = contentType;
-        await httpContext.Response.WriteAsync(json, cancellationToken);
-
-        return true;
     }
     
 
